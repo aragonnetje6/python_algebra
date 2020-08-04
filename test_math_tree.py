da8a3a963ec4398ae03a529385cfdc8475af6f90
Twan <twanstok@gmail.com>
2020-08-04 23:17:59 +0200

    added basic infix tests

--- test_math_tree.py
+++ test_math_tree.py
@@ -53,6 +53,29 @@ class TestAddition:
                         isclose(tree1_derivative.evaluate(variables) + tree2_derivative.evaluate(variables), 1,
                                 abs_tol=1e-09)
 
+    @pytest.mark.parametrize(('rpn1', 'rpn2'), ((x, y) for x in rpn_list for y in rpn_list))
+    def test_infix(self, rpn1, rpn2):
+        tree1 = rpn_to_tree(rpn1)
+        tree2 = rpn_to_tree(rpn2)
+        total = Addition(tree1, tree2)
+        for variables in var_dicts_list:
+            tree1_infix = tree1.infix()
+            tree2_infix = tree2.infix()
+            total_infix = total.infix()
+            for var, val in variables.items():
+                tree1_infix = tree1_infix.replace(var, str(val))
+                tree2_infix = tree2_infix.replace(var, str(val))
+                total_infix = total_infix.replace(var, str(val))
+            try:
+                assert isclose(tree1.evaluate(variables), eval(tree1_infix))
+                assert isclose(tree2.evaluate(variables), eval(tree2_infix))
+                assert isclose(total.evaluate(variables), eval(total_infix))
+            except (ArithmeticError, ValueError):
+                with pytest.raises(Exception):
+                    isclose(tree1.evaluate(variables), 1, abs_tol=1e-09)
+                    isclose(tree2.evaluate(variables), 1, abs_tol=1e-09)
+                    isclose(total.evaluate(variables), 1, abs_tol=1e-09)
+
 
 class TestSubtraction:
     @pytest.mark.parametrize(('rpn1', 'rpn2'), ((x, y) for x in rpn_list for y in rpn_list))
@@ -87,6 +110,29 @@ class TestSubtraction:
                     with pytest.raises(Exception):
                         isclose(tree1_derivative.evaluate(variables) - tree2_derivative.evaluate(variables), 1,
                                 abs_tol=1e-09)
+
+    @pytest.mark.parametrize(('rpn1', 'rpn2'), ((x, y) for x in rpn_list for y in rpn_list))
+    def test_infix(self, rpn1, rpn2):
+        tree1 = rpn_to_tree(rpn1)
+        tree2 = rpn_to_tree(rpn2)
+        total = Subtraction(tree1, tree2)
+        for variables in var_dicts_list:
+            tree1_infix = tree1.infix()
+            tree2_infix = tree2.infix()
+            total_infix = total.infix()
+            for var, val in variables.items():
+                tree1_infix = tree1_infix.replace(var, str(val))
+                tree2_infix = tree2_infix.replace(var, str(val))
+                total_infix = total_infix.replace(var, str(val))
+            try:
+                assert isclose(tree1.evaluate(variables), eval(tree1_infix))
+                assert isclose(tree2.evaluate(variables), eval(tree2_infix))
+                assert isclose(total.evaluate(variables), eval(total_infix))
+            except (ArithmeticError, ValueError):
+                with pytest.raises(Exception):
+                    isclose(tree1.evaluate(variables), 1, abs_tol=1e-09)
+                    isclose(tree2.evaluate(variables), 1, abs_tol=1e-09)
+                    isclose(total.evaluate(variables), 1, abs_tol=1e-09)
 
 
 class TestProduct:
@@ -128,6 +174,29 @@ class TestProduct:
                         ans_der2 = tree2_derivative.evaluate(variables)
                         isclose(ans_der1 * ans2 + ans1 * ans_der2, 1., abs_tol=1e-09)
 
+    @pytest.mark.parametrize(('rpn1', 'rpn2'), ((x, y) for x in rpn_list for y in rpn_list))
+    def test_infix(self, rpn1, rpn2):
+        tree1 = rpn_to_tree(rpn1)
+        tree2 = rpn_to_tree(rpn2)
+        total = Product(tree1, tree2)
+        for variables in var_dicts_list:
+            tree1_infix = tree1.infix()
+            tree2_infix = tree2.infix()
+            total_infix = total.infix()
+            for var, val in variables.items():
+                tree1_infix = tree1_infix.replace(var, str(val))
+                tree2_infix = tree2_infix.replace(var, str(val))
+                total_infix = total_infix.replace(var, str(val))
+            try:
+                assert isclose(tree1.evaluate(variables), eval(tree1_infix))
+                assert isclose(tree2.evaluate(variables), eval(tree2_infix))
+                assert isclose(total.evaluate(variables), eval(total_infix))
+            except (ArithmeticError, ValueError):
+                with pytest.raises(Exception):
+                    isclose(tree1.evaluate(variables), 1, abs_tol=1e-09)
+                    isclose(tree2.evaluate(variables), 1, abs_tol=1e-09)
+                    isclose(total.evaluate(variables), 1, abs_tol=1e-09)
+
 
 class TestDivision:
     @pytest.mark.parametrize(('rpn1', 'rpn2'), ((x, y) for x in rpn_list for y in rpn_list))
@@ -167,6 +236,29 @@ class TestDivision:
                         ans_der1 = tree1_derivative.evaluate(variables)
                         ans_der2 = tree2_derivative.evaluate(variables)
                         isclose((ans_der1 * ans2 + ans1 * ans_der2) / ans2 ** 2, 1., abs_tol=1e-09)
+
+    @pytest.mark.parametrize(('rpn1', 'rpn2'), ((x, y) for x in rpn_list for y in rpn_list))
+    def test_infix(self, rpn1, rpn2):
+        tree1 = rpn_to_tree(rpn1)
+        tree2 = rpn_to_tree(rpn2)
+        total = Division(tree1, tree2)
+        for variables in var_dicts_list:
+            tree1_infix = tree1.infix()
+            tree2_infix = tree2.infix()
+            total_infix = total.infix()
+            for var, val in variables.items():
+                tree1_infix = tree1_infix.replace(var, str(val))
+                tree2_infix = tree2_infix.replace(var, str(val))
+                total_infix = total_infix.replace(var, str(val))
+            try:
+                assert isclose(tree1.evaluate(variables), eval(tree1_infix))
+                assert isclose(tree2.evaluate(variables), eval(tree2_infix))
+                assert isclose(total.evaluate(variables), eval(total_infix))
+            except (ArithmeticError, ValueError):
+                with pytest.raises(Exception):
+                    isclose(tree1.evaluate(variables), 1, abs_tol=1e-09)
+                    isclose(tree2.evaluate(variables), 1, abs_tol=1e-09)
+                    isclose(total.evaluate(variables), 1, abs_tol=1e-09)
 
 
 class TestExponent:
@@ -213,6 +305,29 @@ class TestExponent:
                             raise ArithmeticError
                         isclose(ans1 ** (ans2 - 1) * (ans2 * ans_der1 + ans1 * log(ans1) * ans_der2), 1., abs_tol=1e-09)
 
+    @pytest.mark.parametrize(('rpn1', 'rpn2'), ((x, y) for x in rpn_list for y in rpn_list))
+    def test_infix(self, rpn1, rpn2):
+        tree1 = rpn_to_tree(rpn1)
+        tree2 = rpn_to_tree(rpn2)
+        total = Exponent(tree1, tree2)
+        for variables in var_dicts_list:
+            tree1_infix = tree1.infix()
+            tree2_infix = tree2.infix()
+            total_infix = total.infix()
+            for var, val in variables.items():
+                tree1_infix = tree1_infix.replace(var, str(val))
+                tree2_infix = tree2_infix.replace(var, str(val))
+                total_infix = total_infix.replace(var, str(val))
+            try:
+                assert isclose(tree1.evaluate(variables), eval(tree1_infix))
+                assert isclose(tree2.evaluate(variables), eval(tree2_infix))
+                assert isclose(total.evaluate(variables), eval(total_infix))
+            except (ArithmeticError, ValueError):
+                with pytest.raises(Exception):
+                    isclose(tree1.evaluate(variables), 1, abs_tol=1e-09)
+                    isclose(tree2.evaluate(variables), 1, abs_tol=1e-09)
+                    isclose(total.evaluate(variables), 1, abs_tol=1e-09)
+
 
 class TestLogarithm:
     @pytest.mark.parametrize(('rpn1', 'rpn2'), ((x, y) for x in rpn_list for y in rpn_list))
@@ -256,6 +371,29 @@ class TestLogarithm:
                         isclose(((ans_der1 * log(ans2) / ans1) - (ans_der2 * log(ans1) / ans2)) / log(ans2) ** 2, 1.,
                                 abs_tol=1e-09)
 
+    @pytest.mark.parametrize(('rpn1', 'rpn2'), ((x, y) for x in rpn_list for y in rpn_list))
+    def test_infix(self, rpn1, rpn2):
+        tree1 = rpn_to_tree(rpn1)
+        tree2 = rpn_to_tree(rpn2)
+        total = Logarithm(tree1, tree2)
+        for variables in var_dicts_list:
+            tree1_infix = tree1.infix()
+            tree2_infix = tree2.infix()
+            total_infix = total.infix()
+            for var, val in variables.items():
+                tree1_infix = tree1_infix.replace(var, str(val))
+                tree2_infix = tree2_infix.replace(var, str(val))
+                total_infix = total_infix.replace(var, str(val))
+            try:
+                assert isclose(tree1.evaluate(variables), eval(tree1_infix))
+                assert isclose(tree2.evaluate(variables), eval(tree2_infix))
+                assert isclose(total.evaluate(variables), eval(total_infix))
+            except (ArithmeticError, ValueError):
+                with pytest.raises(Exception):
+                    isclose(tree1.evaluate(variables), 1, abs_tol=1e-09)
+                    isclose(tree2.evaluate(variables), 1, abs_tol=1e-09)
+                    isclose(total.evaluate(variables), 1, abs_tol=1e-09)
+
 
 class TestSine:
     @pytest.mark.parametrize('rpn1', (x for x in rpn_list))
@@ -287,6 +425,19 @@ class TestSine:
                         ans_input = input_tree.evaluate(variables)
                         ans_input_deriv = input_tree_derivative.evaluate(variables)
                         isclose(cos(ans_input) * ans_input_deriv, 1., abs_tol=1e-09)
+
+    @pytest.mark.parametrize('rpn1', (x for x in rpn_list))
+    def test_infix(self, rpn1):
+        total = Sine(rpn_to_tree(rpn1))
+        for variables in var_dicts_list:
+            total_infix = total.infix()
+            for var, val in variables.items():
+                total_infix = total_infix.replace(var, str(val))
+            try:
+                assert isclose(total.evaluate(variables), eval(total_infix))
+            except (ArithmeticError, ValueError):
+                with pytest.raises(Exception):
+                    isclose(total.evaluate(variables), 1, abs_tol=1e-09)
 
 
 class TestCosine:
@@ -320,6 +471,19 @@ class TestCosine:
                         ans_input_deriv = input_tree_derivative.evaluate(variables)
                         isclose(-sin(ans_input) * ans_input_deriv, 1., abs_tol=1e-09)
 
+    @pytest.mark.parametrize('rpn1', (x for x in rpn_list))
+    def test_infix(self, rpn1):
+        total = Cosine(rpn_to_tree(rpn1))
+        for variables in var_dicts_list:
+            total_infix = total.infix()
+            for var, val in variables.items():
+                total_infix = total_infix.replace(var, str(val))
+            try:
+                assert isclose(total.evaluate(variables), eval(total_infix))
+            except (ArithmeticError, ValueError):
+                with pytest.raises(Exception):
+                    isclose(total.evaluate(variables), 1, abs_tol=1e-09)
+
 
 class TestTangent:
     @pytest.mark.parametrize('rpn1', (x for x in rpn_list))
@@ -351,6 +515,19 @@ class TestTangent:
                         ans_input = input_tree.evaluate(variables)
                         ans_input_deriv = input_tree_derivative.evaluate(variables)
                         isclose(ans_input_deriv / cos(ans_input) ** 2, 1., abs_tol=1e-09)
+
+    @pytest.mark.parametrize('rpn1', (x for x in rpn_list))
+    def test_infix(self, rpn1):
+        total = Tangent(rpn_to_tree(rpn1))
+        for variables in var_dicts_list:
+            total_infix = total.infix()
+            for var, val in variables.items():
+                total_infix = total_infix.replace(var, str(val))
+            try:
+                assert isclose(total.evaluate(variables), eval(total_infix))
+            except (ArithmeticError, ValueError):
+                with pytest.raises(Exception):
+                    isclose(total.evaluate(variables), 1, abs_tol=1e-09)
 
 
 class TestArcSine:
@@ -384,6 +561,19 @@ class TestArcSine:
                         ans_input_deriv = input_tree_derivative.evaluate(variables)
                         isclose(ans_input_deriv / (1 - ans_input ** 2) ** .5, 1., abs_tol=1e-09)
 
+    @pytest.mark.parametrize('rpn1', (x for x in rpn_list))
+    def test_infix(self, rpn1):
+        total = ArcSine(rpn_to_tree(rpn1))
+        for variables in var_dicts_list:
+            total_infix = total.infix()
+            for var, val in variables.items():
+                total_infix = total_infix.replace(var, str(val))
+            try:
+                assert isclose(total.evaluate(variables), eval(total_infix))
+            except (ArithmeticError, ValueError):
+                with pytest.raises(Exception):
+                    isclose(total.evaluate(variables), 1, abs_tol=1e-09)
+
 
 class TestArcCosine:
     @pytest.mark.parametrize('rpn1', (x for x in rpn_list))
@@ -415,6 +605,19 @@ class TestArcCosine:
                         ans_input = input_tree.evaluate(variables)
                         ans_input_deriv = input_tree_derivative.evaluate(variables)
                         isclose(-ans_input_deriv / (1 - ans_input ** 2) ** .5, 1., abs_tol=1e-09)
+
+    @pytest.mark.parametrize('rpn1', (x for x in rpn_list))
+    def test_infix(self, rpn1):
+        total = ArcCosine(rpn_to_tree(rpn1))
+        for variables in var_dicts_list:
+            total_infix = total.infix()
+            for var, val in variables.items():
+                total_infix = total_infix.replace(var, str(val))
+            try:
+                assert isclose(total.evaluate(variables), eval(total_infix))
+            except (ArithmeticError, ValueError):
+                with pytest.raises(Exception):
+                    isclose(total.evaluate(variables), 1, abs_tol=1e-09)
 
 
 class TestArcTangent:
@@ -448,6 +651,18 @@ class TestArcTangent:
                         ans_input_deriv = input_tree_derivative.evaluate(variables)
                         isclose(ans_input_deriv / (ans_input ** 2 + 1), 1., abs_tol=1e-09)
 
+    @pytest.mark.parametrize('rpn1', (x for x in rpn_list))
+    def test_infix(self, rpn1):
+        total = ArcTangent(rpn_to_tree(rpn1))
+        for variables in var_dicts_list:
+            total_infix = total.infix()
+            for var, val in variables.items():
+                total_infix = total_infix.replace(var, str(val))
+            try:
+                assert isclose(total.evaluate(variables), eval(total_infix))
+            except (ArithmeticError, ValueError):
+                with pytest.raises(Exception):
+                    isclose(total.evaluate(variables), 1, abs_tol=1e-09)
+
 # todo: add substitution test
-# todo: add infix test
 # todo: add trig functions to tests
