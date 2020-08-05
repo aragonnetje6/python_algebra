5499a410d2840b00e695eb94ced87a06502e789e
Twan <twanstok@gmail.com>
2020-08-05 03:28:36 +0200

    undid class separation

--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -20,29 +20,27 @@ precalc_tuple = [(1, 2, '+'),
                  (((((5, ((5, 'x', '+'), ('y', 4, '+'), '+'), '+'), 2, '*'), 5, '+'), 'x', 'log'),
                   (((5, ('x', 2, '**'), '*'), (4, 'x', '*'), '+'), 3, '+'), 'log')]
 
-precalc_ans = [3, 0, 26, 276, -19736543.53125, 0.1383251008242367]
-
 precalc_var_dict = {'x': 7,
                     'y': 11}
 
 
 class TestConversions:
-    @pytest.mark.parametrize(('entry', 'output'), zip(rpn_list, precalc_tuple))
-    def test_rpn_to_tupletree(self, entry, output):
-        assert rpn_to_tuple(entry) == output
-
-    @pytest.mark.parametrize(('entry', 'output'), zip(precalc_tuple, precalc_ans))
-    def test_tuptree_to_ans(self, entry, output):
-        assert tuple_to_ans(entry, precalc_var_dict) == output
-
-    @pytest.mark.parametrize(('entry', 'output'), zip(rpn_list, precalc_ans))
-    def test_rpn_to_ans(self, entry, output):
-        assert rpn_to_ans(entry, precalc_var_dict) == output
+    @pytest.mark.parametrize('rpn', rpn_list)
+    def test_rpn_to_tuple(self, rpn):
+        assert tuple_to_rpn(rpn_to_tuple(rpn)) == rpn
 
     @pytest.mark.parametrize('entry', precalc_tuple)
-    def test_tuptree_to_nodetree_reversible(self, entry):
+    def test_tuple_to_ans(self, entry):
+        assert tuple_to_ans(entry, precalc_var_dict) == tuple_to_tree(entry).evaluate(precalc_var_dict)
+
+    @pytest.mark.parametrize('rpn', rpn_list)
+    def test_rpn_to_ans(self, rpn):
+        assert rpn_to_ans(rpn, precalc_var_dict) == rpn_to_tree(rpn).evaluate(precalc_var_dict)
+
+    @pytest.mark.parametrize('entry', precalc_tuple)
+    def test_tuple_to_tree_reversible(self, entry):
         assert tuple_to_tree(entry).tuple() == entry
 
     @pytest.mark.parametrize('rpn', rpn_list)
-    def test_rpn_to_nodetree_reversible(self, rpn):
+    def test_rpn_to_tree_reversible(self, rpn):
         assert rpn_to_tree(rpn).rpn() == rpn
