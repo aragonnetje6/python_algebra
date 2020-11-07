d750dd79a335cc428e5d046cfba94f21a10b5c95
aragonnetje6 <twanstok@gmail.com>
2020-11-07 23:23:47 +0100

    improved GUI

--- gui.py
+++ gui.py
@@ -4,8 +4,9 @@ User interface for the tree
 
 from os import system
 
+from IPython import get_ipython
 from IPython.display import HTML, display_html
-from math_tree import Node, tag
+from math_tree import *
 
 
 def generate_html_doc(expression: Node) -> str:
@@ -21,17 +22,17 @@ def generate_html_doc(expression: Node) -> str:
                            'xmlns = "http://www.w3.org/1998/Math/MathML" id = "expr"')))
 
 
-def display_ipython(expression: Node) -> None:
-    """generates html code for expression"""
-    # noinspection PyTypeChecker
-    display_html(HTML(tag('math',
-                          expression.mathml(),
-                          'xmlns = "http://www.w3.org/1998/Math/MathML" id = "expr"')))
-
-
 def display(expression: Node) -> None:
     """Generates and opens html representation of expression"""
-    html = generate_html_doc(expression)
-    with open('output.html', 'w') as file:
-        file.write(html)
-    system('output.html')
+    if get_ipython().__class__.__name__ == 'ZMQInteractiveShell':
+        # noinspection PyTypeChecker
+        display_html(
+            HTML(
+                tag('math',
+                    expression.mathml(),
+                    'xmlns = "http://www.w3.org/1998/Math/MathML" id = "expr"')))
+    else:
+        html = generate_html_doc(expression)
+        with open('output.html', 'w') as file:
+            file.write(html)
+        system('output.html')
