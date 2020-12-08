fa9e355eb70f668f83671f0193c3850b57f648a5
aragonnetje6 <twanstok@gmail.com>
2020-12-08 19:40:51 +0100

    fixed mypy typing of tests

--- test_math_tree.py
+++ test_math_tree.py
@@ -8,22 +8,22 @@ from hypothesis.strategies import SearchStrategy, deferred, one_of, builds, samp
 from math_tree import Node, Constant, Variable, Addition, Subtraction, Product, Division, Exponent, Logarithm, Equal, \
     NotEqual, GreaterThan, LessThan, GreaterEqual, LessEqual, And, Or, Nand, Nor, Xor, Xnor, Sine, Cosine, Tangent, \
     ArcSine, ArcCosine, ArcTangent, Absolute, Negate, Invert, Not, Derivative, IndefiniteIntegral, DefiniteIntegral, \
-    Piecewise
+    Piecewise, Variables
 from pytest import fixture
 
 
 @fixture(scope="module")
-def x():
+def x() -> Variable:
     return Variable('x')
 
 
 @fixture(scope="module")
-def y():
+def y() -> Variable:
     return Variable('y')
 
 
 @fixture(scope="module")
-def z():
+def z() -> Variable:
     return Variable('z')
 
 
@@ -36,7 +36,7 @@ calculus_operators = [Derivative, IndefiniteIntegral, DefiniteIntegral]
 misc_operators = [Piecewise]
 
 
-def variables_dict(keys: str, bools: bool = False) -> SearchStrategy:
+def variables_dict(keys: str, bools: bool = False) -> SearchStrategy[Variables]:
     if bools:
         return dictionaries(sampled_from(keys), booleans())
     else:
@@ -49,20 +49,21 @@ constant_number = builds(Constant, one_of(integers(), floats(allow_nan=False, al
 constant_bool = builds(Constant, booleans())
 constant_any = one_of(constant_bool, constant_number)
 variable = builds(Variable, sampled_from('xyz'))
-math_expression = deferred(lambda: (constant_number
-                                    | variable
-                                    | one_of(*[builds(operator, math_expression) for operator in unary_operators])
-                                    | one_of(*[builds(operator, math_expression, math_expression)
-                                               for operator in binary_operators])))
+func = lambda: (constant_number
+                | variable
+                | one_of(*[builds(operator, math_expression) for operator in unary_operators])
+                | one_of(*[builds(operator, math_expression, math_expression)
+                           for operator in binary_operators]))
+math_expression = deferred(func)  # type: SearchStrategy[Node]
 
 
 @given(val1=constant_any, val2=constant_any)
-def test_equality(val1: Node, val2: Node):
-    assert (val1 == val2).evaluate() == (val1.evaluate() == val2.evaluate())
+def test_equality(val1: Node, val2: Node) -> None:
+    assert Equal(val1, val2).evaluate() == (val1.evaluate() == val2.evaluate())
 
 
 @given(expr=math_expression, var_dict=variables_dict('xyz'))
-def test_no_floats(expr, var_dict):
+def test_no_floats(expr: Node, var_dict: Variables) -> None:
     try:
         assert not isinstance(expr.evaluate(var_dict), float)
     except (OverflowError, ValueError, ArithmeticError):
@@ -71,104 +72,104 @@ def test_no_floats(expr, var_dict):
 
 class TestIdentities:
     @given(var_dict=variables_dict('xy'))
-    def test_1(self, x: Variable, y: Variable, var_dict):
-        assert (x + y == y + x).evaluate(var_dict)
+    def test_1(self, x: Variable, y: Variable, var_dict: Variables) -> None:
+        assert Equal(x + y, y + x).evaluate(var_dict)
 
     @given(var_dict=variables_dict('xyz'))
-    def test_2(self, x: Variable, y: Variable, z: Variable, var_dict):
-        assert ((x + y) + z == x + (y + z)).evaluate(var_dict)
+    def test_2(self, x: Variable, y: Variable, z: Variable, var_dict: Variables) -> None:
+        assert Equal((x + y) + z, x + (y + z)).evaluate(var_dict)
 
     @given(var_dict=variables_dict('x'))
-    def test_3(self, x: Variable, var_dict):
-        assert (x + 0 == x).evaluate(var_dict)
+    def test_3(self, x: Variable, var_dict: Variables) -> None:
+        assert Equal(x + 0, x).evaluate(var_dict)
 
     @given(var_dict=variables_dict('x'))
-    def test_4(self, x: Variable, var_dict):
-        assert (x + -x == 0).evaluate(var_dict)
+    def test_4(self, x: Variable, var_dict: Variables) -> None:
+        assert Equal(x + -x, Constant(0)).evaluate(var_dict)
 
     @given(var_dict=variables_dict('xy'))
-    def test_5(self, x: Variable, y: Variable, var_dict):
-        assert (x + -y == x - y).evaluate(var_dict)
+    def test_5(self, x: Variable, y: Variable, var_dict: Variables) -> None:
+        assert Equal(x + -y, x - y).evaluate(var_dict)
 
     @given(var_dict=variables_dict('x'))
-    def test_6(self, x: Variable, var_dict):
-        assert (x - 0 == x).evaluate(var_dict)
+    def test_6(self, x: Variable, var_dict: Variables) -> None:
+        assert Equal(x - 0, x).evaluate(var_dict)
 
     @given(var_dict=variables_dict('x'))
-    def test_7(self, x: Variable, var_dict):
-        assert (x + x == x * 2).evaluate(var_dict)
+    def test_7(self, x: Variable, var_dict: Variables) -> None:
+        assert Equal(x + x, x * 2).evaluate(var_dict)
 
     @given(var_dict=variables_dict('xy'))
-    def test_8(self, x: Variable, y: Variable, var_dict):
-        assert (x * y == y * x).evaluate(var_dict)
+    def test_8(self, x: Variable, y: Variable, var_dict: Variables) -> None:
+        assert Equal(x * y, y * x).evaluate(var_dict)
 
     @given(var_dict=variables_dict('xyz'))
-    def test_9(self, x: Variable, y: Variable, z: Variable, var_dict):
+    def test_9(self, x: Variable, y: Variable, z: Variable, var_dict: Variables) -> None:
         try:
-            assert ((x * y) * z == x * (y * z)).evaluate(var_dict)
+            assert Equal((x * y) * z, x * (y * z)).evaluate(var_dict)
         except OverflowError:
             pass
 
     @given(var_dict=variables_dict('xyz'))
-    def test_10(self, x: Variable, y: Variable, z: Variable, var_dict):
+    def test_10(self, x: Variable, y: Variable, z: Variable, var_dict: Variables) -> None:
         try:
-            assert (x * (y + z) == x * y + x * z).evaluate(var_dict)
+            assert Equal(x * (y + z), x * y + x * z).evaluate(var_dict)
         except OverflowError:
             pass
 
     @given(var_dict=variables_dict('x'))
-    def test_11(self, x: Variable, var_dict):
-        assert (x * 1 == x).evaluate(var_dict)
+    def test_11(self, x: Variable, var_dict: Variables) -> None:
+        assert Equal(x * 1, x).evaluate(var_dict)
 
     @given(var_dict=variables_dict('x'))
-    def test_12(self, x: Variable, var_dict):
-        assert (x * 0 == 0).evaluate(var_dict)
+    def test_12(self, x: Variable, var_dict: Variables) -> None:
+        assert Equal(x * 0, Constant(0)).evaluate(var_dict)
 
     @given(var_dict=variables_dict('x'))
-    def test_13(self, x: Variable, var_dict):
+    def test_13(self, x: Variable, var_dict: Variables) -> None:
         if x.evaluate(var_dict) != 0:
-            assert (x * Invert(x) == 1).evaluate(var_dict)
+            assert Equal(x * Invert(x), Constant(1)).evaluate(var_dict)
 
     @given(var_dict=variables_dict('xy'))
-    def test_14(self, x: Variable, y: Variable, var_dict):
+    def test_14(self, x: Variable, y: Variable, var_dict: Variables) -> None:
         if y.evaluate(var_dict) != 0:
-            assert (x * Invert(y) == x / y).evaluate(var_dict)
+            assert Equal(x * Invert(y), x / y).evaluate(var_dict)
 
     @given(var_dict=variables_dict('x'))
-    def test_15(self, x: Variable, var_dict):
-        assert (x / 1 == x).evaluate(var_dict)
+    def test_15(self, x: Variable, var_dict: Variables) -> None:
+        assert Equal(x / 1, x).evaluate(var_dict)
 
     @given(var_dict=variables_dict('x'))
-    def test_16(self, x: Variable, var_dict):
+    def test_16(self, x: Variable, var_dict: Variables) -> None:
         try:
-            assert (x * x == x ** 2).evaluate(var_dict)
+            assert Equal(x * x, x ** 2).evaluate(var_dict)
         except OverflowError:
             pass
 
     @given(var_dict=variables_dict('x'))
-    def test_17(self, x: Variable, var_dict):
-        assert ((x ** 1 == x) | (x == 0)).evaluate(var_dict)
+    def test_17(self, x: Variable, var_dict: Variables) -> None:
+        assert (Equal(x ** 1, x) | Equal(x, Constant(0))).evaluate(var_dict)
 
     @given(var_dict=variables_dict('x'))
-    def test_18(self, x: Variable, var_dict):
-        assert (x ** 0 == 1).evaluate(var_dict)
+    def test_18(self, x: Variable, var_dict: Variables) -> None:
+        assert Equal(x ** 0, Constant(1)).evaluate(var_dict)
 
     @given(var_dict=variables_dict('xy'))
-    def test_19(self, x: Variable, y: Variable, var_dict):
+    def test_19(self, x: Variable, y: Variable, var_dict: Variables) -> None:
         try:
-            assert ((x + y) ** 2 == x ** 2 + y ** 2 + 2 * x * y).evaluate(var_dict)
+            assert Equal((x + y) ** 2, x ** 2 + y ** 2 + 2 * x * y).evaluate(var_dict)
         except OverflowError:
             pass
 
     @given(var_dict=variables_dict('x'))
-    def test_20(self, x: Variable, var_dict):
+    def test_20(self, x: Variable, var_dict: Variables) -> None:
         try:
-            assert (Logarithm(x, x) == 1).evaluate(var_dict)
+            assert Equal(Logarithm(x, x), Constant(1)).evaluate(var_dict)
         except ValueError:
             assert x.evaluate(var_dict) == 0 or x.evaluate(var_dict) == 1
 
     # @given(var_dict=variables_dict('xy'))
-    # def test_21(self, x: Variable, y: Variable, var_dict):
+    # def test_21(self, x: Variable, y: Variable, var_dict: Variables) -> None:
     #     try:
     #         x1, y1 = x.evaluate(var_dict), (x ** y).evaluate(var_dict)
     #         if (0 < y1 < 1 and x1 > 0) or (y1 > 1 and x1 > 0) or (y1 != 0 and y1 != 1 and x1 == y1):
@@ -177,7 +178,7 @@ class TestIdentities:
     #         pass
     #
     # @given(var_dict=variables_dict('xy'))
-    # def test_22(self, x: Variable, y: Variable, var_dict):
+    # def test_22(self, x: Variable, y: Variable, var_dict: Variables) -> None:
     #     try:
     #         x1, y1 = x.evaluate(var_dict), y.evaluate(var_dict)
     #         if (0 < y1 < 1 or y1 > 1) and x1 > 0:
@@ -188,7 +189,7 @@ class TestIdentities:
 # class TestTransformation:
 #     @settings(deadline=10000)
 #     @given(expr=math_expression)
-#     def test_simplify(self, expr: Node):
+#     def test_simplify(self, expr: Node) -> None:
 #         try:
 #             assert expr.evaluate() == expr.simplify().evaluate()
 #         except (ValueError, ArithmeticError):
@@ -196,7 +197,7 @@ class TestIdentities:
 #
 #     @settings(deadline=10000)
 #     @given(expr=math_expression)
-#     def test_polynomial(self, expr: Node):
+#     def test_polynomial(self, expr: Node) -> None:
 #         try:
 #             assert expr.evaluate() == expr.polynomial().evaluate()
 #         except (ValueError, ArithmeticError):
