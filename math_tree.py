709a94bb9516910c0a323c104da890595982e88f
Twan Stok <twanstok@gmail.com>
2021-03-25 01:49:57 +0100

    undid test 2
    Signed-off-by: aragonnetje6 <twanstok@gmail.com>

--- math_tree.py
+++ math_tree.py
@@ -2069,6 +2069,3 @@ class Piecewise(Node):
         """return wolfram language representation of the tree"""
         expressions = ', '.join(f'{{{expr.wolfram()}, {cond.wolfram()}}}' for expr, cond in self.expressions)
         return f'{self.wolfram_func}[{{{expressions}}}, {self.default.wolfram()}]'
-
-
-test
