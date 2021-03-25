0db859facbe85393178cabb2582558f41a9bfa94
aragonnetje6 <twanstok@gmail.com>
2021-03-25 02:18:37 +0100

    __eq__ method now checks for equality of simplified forms
    Signed-off-by: aragonnetje6 <twanstok@gmail.com>

--- math_tree.py
+++ math_tree.py
@@ -121,7 +121,7 @@ class Node(metaclass=ABCMeta):
 
     def __eq__(self, other: Any) -> bool:
         # todo: find better way to do this, temp fix
-        return self is other or repr(self) == repr(Nodeify(other))
+        return self is other or repr(self.simplify()) == repr(Nodeify(other).simplify())
 
     def __hash__(self) -> int:
         return hash(repr(self))
